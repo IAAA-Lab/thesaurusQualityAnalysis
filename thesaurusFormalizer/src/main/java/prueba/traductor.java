241bd798487e930f0a73fe9910245e4858905f44
jlacasta <jlacasta@DESKTOP-EC9EU65>
2018-07-03 10:55:59 +0200

    cambio en el traductor

--- thesaurusFormalizer/src/main/java/prueba/traductor.java
+++ thesaurusFormalizer/src/main/java/prueba/traductor.java
@@ -17,59 +17,56 @@ import org.apache.jena.rdf.model.Statement;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class traductor {
-	
+
 	@Autowired
-	public static void main(String[] args) throws Exception {		
+	public static void main(String[] args) throws Exception {
+
+		Model model = ModelFactory.createDefaultModel();
+		model.read("data/input/thesauri/openUp.ttl", "TURTLE");
+		List<Resource> concepts = model.listSubjects().toList();
+		FileWriter fw = new FileWriter("data/input/thesauri/openUpEx.ttl");
+		String label_obj;
+		String lang_esp = "Language: Spanish";
+		String lang_es = "Language: es";
+		String lang_en = "Language: en";
+		String lang_eng = "Language: English";
 		
-		  Model model = ModelFactory.createDefaultModel(); 	
-		  model.read("data/input/thesauri/openUp.ttl","TURTLE");
-	      List<Resource> concepts = model.listSubjects().toList();
-	      FileWriter fw= new FileWriter("d:\\oepnUp.ttl");
-	      String label_obj;	     
-	      String lang_esp="Language: Spanish";
-	      String lang_es="Language: es";
-	      String lang_en="Language: en";
-	      String lang_eng="Language: English";
-	      String lang_eu="Language: eu";
-	      String lang_fr="Language: fr";
-	      String lang_fren="Language: french";
-	      String value;
-	    
-	      Property Note=model.createProperty("http://www.w3.org/2004/02/skos/core#note");
-	      Property PrefLabelProp=model.createProperty("http://www.w3.org/2004/02/skos/core#prefLabel");
+		String lang_fr = "Language: fr";
+		String lang_fren = "Language: french";
+		String value;
 
-	      for (Resource res : concepts) {
-	    	for (Statement st : res.listProperties(Note).toList()) {
-	    			    				
-	            label_obj=st.getObject().toString();	    		
-	    		    		
-	    		if(label_obj.equals(lang_en)||label_obj.equals(lang_eng)||label_obj.equals(lang_eu)) {
-    			value= res.getProperty(PrefLabelProp).getObject().toString();
-    			res.removeAll(PrefLabelProp);
-    			res.addProperty(PrefLabelProp, value, "en");
-    			System.out.println(res.getProperty(PrefLabelProp).getObject().toString()); 			  
-    			
-    			} 
-	    		if(label_obj.equals(lang_es)||label_obj.equals(lang_esp)) {
-	    			value= res.getProperty(PrefLabelProp).getObject().toString();
-	    			res.removeAll(PrefLabelProp);
-	    			res.addProperty(PrefLabelProp, value, "es");
-	    			System.out.println(res.getProperty(PrefLabelProp).getObject().toString()); 			  
-	    			
-	    		} 
-	    		if(label_obj.equals(lang_fr)||label_obj.equals(lang_fren)) {
-	    			value= res.getProperty(PrefLabelProp).getObject().toString();
-	    			res.removeAll(PrefLabelProp);
-	    			res.addProperty(PrefLabelProp, value, "fr");
-	    			System.out.println(res.getProperty(PrefLabelProp).getObject().toString()); 			  
-	    			
-	    		} 	    		
-	    	}  		
-	      }   
-	           model.write(fw);
-  		       fw.close();
-	      }   
-	 }
+		Property Note = model.createProperty("http://www.w3.org/2004/02/skos/core#note");
+		Property PrefLabelProp = model.createProperty("http://www.w3.org/2004/02/skos/core#prefLabel");
 
+		for (Resource res : concepts) {
+			for (Statement st : res.listProperties(Note).toList()) {
 
+				label_obj = st.getObject().toString();
 
+				if (label_obj.equals(lang_en) || label_obj.equals(lang_eng) ) {
+					value = res.getProperty(PrefLabelProp).getObject().toString();
+					res.removeAll(PrefLabelProp);
+					res.addProperty(PrefLabelProp, value, "en");
+					// System.out.println(res.getProperty(PrefLabelProp).getObject().toString());
+
+				}
+				if (label_obj.equals(lang_es) || label_obj.equals(lang_esp)) {
+					value = res.getProperty(PrefLabelProp).getObject().toString();
+					res.removeAll(PrefLabelProp);
+					res.addProperty(PrefLabelProp, value, "es");
+					// System.out.println(res.getProperty(PrefLabelProp).getObject().toString());
+
+				}
+				if (label_obj.equals(lang_fr) || label_obj.equals(lang_fren)) {
+					value = res.getProperty(PrefLabelProp).getObject().toString();
+					res.removeAll(PrefLabelProp);
+					res.addProperty(PrefLabelProp, value, "fr");
+					// System.out.println(res.getProperty(PrefLabelProp).getObject().toString());
+
+				}
+			}
+		}
+		model.write(fw);
+		fw.close();
+	}
+}
